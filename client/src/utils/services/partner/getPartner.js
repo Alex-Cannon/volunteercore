57d92dff560e3d4c0b09930421ac2f79d72efe25
alex-cannon <alexcannon2018@gmail.com>
2019-09-04 15:23:50 -0600

    bug fix: fixed put partner

--- client/src/utils/services/partner/getPartner.js
+++ client/src/utils/services/partner/getPartner.js
@@ -4,16 +4,16 @@ import { store } from '../../../redux/store';
 import { loadingGetPartner, errorGetPartner, successGetPartner } from '../../../redux/actions';
 const dispatch = store.dispatch;
 
-export const getPartner = (id, callback = ()=>{}) => {
+export const getPartner = (id, callback) => {
   dispatch(loadingGetPartner());
 
   axios.get(`/api/partners/${id}`)
     .then(({ data }) => {
-      if (!callback) dispatch(successGetPartner(data));
+      if (!callback) return dispatch(successGetPartner(data));
       callback(data, null);
     })
     .catch(error => {
-      if (!callback) dispatch(errorGetPartner(error));
+      if (!callback) return dispatch(errorGetPartner(error));
       callback(null, error);
     });
 }
--- client/src/utils/services/partner/putPartner.js
+++ client/src/utils/services/partner/putPartner.js
@@ -4,16 +4,16 @@ import { store } from '../../../redux/store';
 import { loadingPostPartner, successPostPartner, errorPostPartner } from '../../../redux/actions';
 const dispatch = store.dispatch;
 
-export const putPartner = (data, callback = ()=>{}) => {
+export const putPartner = (data, callback) => {
   dispatch(loadingPostPartner());
 
   axios.put(`/api/partners/${data.id}`, data)
     .then(({ data }) => {
-      if (!callback) dispatch(successPostPartner());
+      if (!callback) return dispatch(successPostPartner());
       callback(data, null);
     })
     .catch(error => {
-      if (!callback) dispatch(errorPostPartner(error));
+      if (!callback) return dispatch(errorPostPartner(error));
       callback(null, error);
     });
 }
