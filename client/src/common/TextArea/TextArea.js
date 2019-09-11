02e7319b80c9b23856c1d1d5d07b9b3a067fd3f1
alex-cannon <alexcannon2018@gmail.com>
2019-09-11 15:20:44 -0600

    made textarea useable

--- client/src/common/TextArea/TextArea.js
+++ client/src/common/TextArea/TextArea.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './TextArea.scss';
 
 export const TextArea = (props) => {
-  const { label, required } = props;
+  const { label, placeholder, required, setValue, value } = props;
 
   return (
     <div className="input-group">
       {label ? <label>{ (required ? "* " : "") + label}</label> : ""}
       <textarea
         className="textarea"
-        {...props}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder={placeholder}
+        value={value}
       />
     </div>
   );
